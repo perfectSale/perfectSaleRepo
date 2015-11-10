a1e98ab13fc19a7e894c720ada138ff5de04ac42
fadih <fadih@perfectomobile.com>
2015-11-10 20:12:23 +0200

    updated

--- perfectSale/app/Global.java
+++ perfectSale/app/Global.java
@@ -1,7 +1,11 @@
+import com.google.inject.Inject;
+
 import play.*;
 import play.api.mvc.Handler;
 import play.libs.F.Promise;
+import play.libs.ws.WSClient;
 import play.mvc.*;
+import views.html.pageNotFound;
 import static play.mvc.Results.*;
 
 
@@ -10,6 +14,8 @@ import static play.mvc.Results.*;
 public class Global extends GlobalSettings  {
 	
 
+	
+	
 	 @Override
 	  public void onStart(Application app) {
 	    Logger.info("Application has started");
@@ -24,21 +30,27 @@ public class Global extends GlobalSettings  {
 	   @Override
 	    public Promise<Result> onBadRequest(Http.RequestHeader request, java.lang.String error) {
 	        Logger.info("onBadRequest");
-	        return null;
+	        return Promise.<Result>pure(badRequest("Don't try to hack the URI!"));
 	      
 	    }
 	  
 	    @Override
 	   public Promise<Result> onError(Http.RequestHeader request, java.lang.Throwable t){
 		   Logger.info("onError");
-		   return null;
+		   return Promise.<Result>pure(notFound(
+		             views.html.pageNotFound.render()
+		         ));
 		   
 	   }
 	
 	 @Override
       public Promise<Result> onHandlerNotFound(Http.RequestHeader request){
-	     Logger.info("onHandlerNotFound");
-		return null;
+	     Logger.info("onHandlerNotFound");	          
+	    
+	     return Promise.<Result>pure(notFound(
+	             views.html.pageNotFound.render()
+	         ));
+
 	   }
 	   
 	   
--- perfectSale/app/controllers/Application.java
+++ perfectSale/app/controllers/Application.java
@@ -1,14 +1,21 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import com.google.inject.Inject;
 
+import play.*;
+import play.libs.ws.WSClient;
+import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
 
+
+	 
     public Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(pageNotFound.render());
     }
+    
+  
+    
 
 }
